83016f18ebd570207e1528041d7d0559526d5280
alife-echo <alifesilva90@gmail.com>
2023-02-07 20:59:10 -0300

    menu terminado e otimizado

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let screenWidth = window.innerWidth;
 document.getElementById('btnMenu').addEventListener('click',(e)=>{
     e.preventDefault()
         document.getElementById('sideBar').style.width = '20%'
-})
-document.getElementById('btnClose').addEventListener('click',()=>{
-   
-    document.getElementById('sideBar').style.width = '0%'
-    
-
+        document.getElementById('shadowBar').style.width = '100%'
+       
 })
 
-
+let elements = document.querySelectorAll('#sideBar,#shadowBar')
+for(let i = 0;i<elements.length;i++){
+     elements[i].addEventListener('click',()=>{
+        document.getElementById('sideBar').style.width = '0%'
+        document.getElementById('shadowBar').style.width = '0%'
+     })
+}
