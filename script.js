6a3b24f193821cbc5d410c1ea98b0b7efc81b510
alife-echo <alifesilva90@gmail.com>
2023-02-06 22:04:43 -0300

    criando menu

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.getElementById('btnMenu').addEventListener('click',e=>{
+    e.preventDefault()
+        document.getElementById('main').style.marginLeft = '25rem'
+        document.getElementById('sideBar').style.width = '25rem'
+    
+})
+
