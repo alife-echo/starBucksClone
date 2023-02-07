7064a7c958ef5d6aa3182e59990f95d70dba80b0
alife-echo <alifesilva90@gmail.com>
2023-02-07 16:42:56 -0300

    adicionando footer e alterações no menu

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-
 document.getElementById('btnMenu').addEventListener('click',e=>{
     e.preventDefault()
         document.getElementById('main').style.marginLeft = '25rem'
         document.getElementById('sideBar').style.width = '25rem'
-    
+        document.getElementById('sideBar').style.display = 'block'
 })
+document.getElementById('btnClose').addEventListener('click',()=>{
+    document.getElementById('main').style.marginLeft = '0rem'
+    document.getElementById('sideBar').style.width = '0rem'
+    document.getElementById('sideBar').style.display = 'none'
+})
+
 
