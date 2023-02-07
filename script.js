aac8fdaf4c66e9a0f96766d69dd4cc3bf771e86c
alife-echo <alifesilva90@gmail.com>
2023-02-07 18:34:36 -0300

    alterações js

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-document.getElementById('btnMenu').addEventListener('click',e=>{
+let screenWidth = window.innerWidth;
+var apresentation = document.querySelector('.containerFlex')
+var boxFlexColumn = document.querySelector('.containerFlexColumn')
+var brotherContainer = document.querySelector('.brotherContainer')
+document.getElementById('btnMenu').addEventListener('click',(e)=>{
     e.preventDefault()
         document.getElementById('main').style.marginLeft = '25rem'
         document.getElementById('sideBar').style.width = '25rem'
         document.getElementById('sideBar').style.display = 'block'
+        
+        if (screenWidth <= 1080) {
+            // Aplique estilos para dispositivos móveis aqui
+            apresentation.className = 'mediaApresentation'
+            boxFlexColumn.className = 'imgsOneContainer'
+            brotherContainer.className = 'imgsOneContainer'
+        } else {
+            // Aplique estilos para desktops aqui
+        }
 })
 document.getElementById('btnClose').addEventListener('click',()=>{
     document.getElementById('main').style.marginLeft = '0rem'
     document.getElementById('sideBar').style.width = '0rem'
     document.getElementById('sideBar').style.display = 'none'
+    apresentation.className = 'containerFlex'
+    boxFlexColumn.className = 'containerFlexColumn'
+    brotherContainer.className = 'brotherContainer'
+
 })
 
 
